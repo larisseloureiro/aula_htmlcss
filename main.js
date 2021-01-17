64f429b495543e1399c1d4721e62cd26144b8a6e
Larisse Loureiro <larisseloureirob@gmail.com>
2021-01-16 23:02:25 -0300

    Projeto com JS

---
+++ main.js
@@ -0,0 +1,11 @@
+function redirecionarGmail() {
+  window.open("mailto:larisseloureirob@gmail.com")
+}
+function redirecionarGit() {
+  window.open("https://github.com/larisseloureiro")
+}
+function redirecionarLink() {
+  window.open("https://www.linkedin.com/in/larisse-loureiro/")
+}
+
+
